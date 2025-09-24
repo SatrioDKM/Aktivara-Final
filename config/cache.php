61649036f5fdebcf356777b8ec0476be4d5138b6
hndko <hd.doko22@gmail.com>
2025-09-24 21:53:31 +0700

    chore: update default env and config settings
    - Updated default DB connection
    - Enabled session encryption
    - Removed maintenance store
    - Updated queue work command

--- config/cache.php
+++ config/cache.php
@@ -43,7 +43,7 @@ return [
             'connection' => env('DB_CACHE_CONNECTION'),
             'table' => env('DB_CACHE_TABLE', 'cache'),
             'lock_connection' => env('DB_CACHE_LOCK_CONNECTION'),
-            'lock_table' => env('DB_CACHE_LOCK_TABLE'),
+            'lock_table' => env('DB_CACHE_LOCK_TABLE', 'cache_locks'),
         ],
 
         'file' => [
@@ -103,6 +103,6 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache_'),
+    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_cache_'),
 
 ];

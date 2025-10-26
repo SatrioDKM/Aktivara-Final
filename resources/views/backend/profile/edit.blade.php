3f74df17db0756d31f71855edbe961acfc384f7f
SatrioDKM <Satriodkm@hotmail.com>
2025-10-26 19:07:02 +0700

    hilangkan menu hapus akun
    hilangkan menu hapus akun

--- resources/views/backend/profile/edit.blade.php
+++ resources/views/backend/profile/edit.blade.php
@@ -219,65 +219,6 @@
                 </div>
             </div>
 
-            {{-- Bagian Hapus Akun --}}
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow-lg sm:rounded-lg">
-                <div class="max-w-xl">
-                    <section class="space-y-6">
-                        <header>
-                            <h2 class="text-lg font-medium text-red-600 dark:text-red-400">
-                                {{ __('Hapus Akun') }}
-                            </h2>
-                            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                                {{ __('Setelah akun Anda dihapus, semua sumber daya dan datanya akan dihapus secara
-                                permanen. Sebelum menghapus, harap unduh data apa pun yang ingin Anda simpan.') }}
-                            </p>
-                        </header>
-
-                        <x-danger-button x-data=""
-                            x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">
-                            <i class="fas fa-trash-alt me-2"></i>
-                            {{ __('Hapus Akun') }}
-                        </x-danger-button>
-
-                        <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
-                            <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
-                                @csrf
-                                @method('delete')
-
-                                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                                    {{ __('Apakah Anda yakin ingin menghapus akun Anda?') }}
-                                </h2>
-                                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                                    {{ __('Setelah akun Anda dihapus, semua sumber daya dan datanya akan dihapus secara
-                                    permanen. Silakan masukkan password Anda untuk mengonfirmasi penghapusan permanen
-                                    ini.') }}
-                                </p>
-
-                                <div class="mt-6">
-                                    <x-input-label for="password_delete" value="{{ __('Password') }}" class="sr-only" />
-                                    <div class="relative mt-1">
-                                        <div
-                                            class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                            <i class="fas fa-key text-gray-400"></i></div>
-                                        <x-text-input id="password_delete" name="password" type="password"
-                                            class="block w-full ps-10" placeholder="{{ __('Password') }}" />
-                                    </div>
-                                    <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
-                                </div>
-
-                                <div class="mt-6 flex justify-end">
-                                    <x-secondary-button x-on:click="$dispatch('close')">
-                                        {{ __('Batal') }}
-                                    </x-secondary-button>
-                                    <x-danger-button class="ms-3">
-                                        {{ __('Hapus Akun') }}
-                                    </x-danger-button>
-                                </div>
-                            </form>
-                        </x-modal>
-                    </section>
-                </div>
-            </div>
         </div>
     </div>
 
